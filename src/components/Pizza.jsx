903d2d1706030e409000f899f761bfd202b49b4b
Craig Puxty <craig_puxty@talktalk.net>
2023-11-28 21:59:46 +0000

    Added pizza images, Pizza component & pizzaData. Updated index.js

---
+++ src/components/Pizza.jsx
@@ -0,0 +1,9 @@
+export default function Pizza() {
+  return (
+    <div>
+      <img src='images/pizzas/focaccia.jpg' alt='Focaccia' />
+      <h2>Focaccia</h2>
+      <p>Bread with italian olive oil and rosemary</p>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+// Components
+import Pizza from './components/Pizza';
+
+// Data
+import { pizzaData } from './data/pizzaData';
+
 export default function App() {
-  return <h1>Pizza menu</h1>;
+  return (
+    <div>
+      <h1>Pizza menu</h1>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
