4bd8c1340b0124f4edde35ef5d3c2ba2aa626124
Craig Puxty <craig_puxty@talktalk.net>
2023-11-29 13:51:02 +0000

    Added conditional paragraph to menu.

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -8,12 +8,19 @@ export default function Menu() {
   return (
     <main className='menu'>
       <h2>Our menu</h2>
+
       {pizzaData.length > 0 ? (
-        <ul className='pizzas'>
-          {pizzaData.map((pizza, i) => (
-            <Pizza pizza={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+          <ul className='pizzas'>
+            {pizzaData.map((pizza, i) => (
+              <Pizza pizza={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We're still working on our menu. Please come back later.</p>
       )}
